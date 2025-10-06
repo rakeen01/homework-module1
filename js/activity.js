a254c6a80b6375f278a4a8f7996c4a9c80548c31
Md Rakeen Murtaza <rakeen.murtaza.12@gmail.com>
2025-10-05 23:05:12 -0700

    week 5 day 1 - commit 2

--- js/activity.js
+++ js/activity.js
@@ -1,1 +1,16 @@
-$(document).ready(function () { });
+
+$(document).ready(function () {
+
+    // Mark cells that are selectable (all body <td> except “Not Available”)
+    $('tbody td').each(function () {
+        if ($(this).text().trim().toLowerCase() !== 'not available') {
+            $(this).addClass('selectable');
+        }
+    });
+
+    // Toggle highlight on click; multiple cells can be selected
+    $('tbody td.selectable').on('click', function () {
+        $(this).toggleClass('selected');
+    });
+
+});
